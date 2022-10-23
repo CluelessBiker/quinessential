a4ccbb5cd51b88c8f6fa07622ba9ddfd721c5eda
Lauren-Nicole <popich.red@gmail.com>
2022-10-23 09:16:16 +0000

    Update app & create style sheet.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import './App.module.css';
+import Login from './pages/login/Login';
+import MainPage from './pages/mainpage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" render={() => <MainPage />} />
+        <Route path="/login" render={() => <Login />} />
+        <Route render={() => <h3>Page Not Found.</h3>} />
+      </Switch>
     </div>
   );
 }
